20deeadf037b931a7af758493f51255a124f65d6
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-12 21:21:29 -0700

    Completed reverse string

--- DSA-recursion.js
+++ DSA-recursion.js
@@ -10,11 +10,10 @@ const countSheep = function(num) {
   return `${num} - Another sheep jump over the fence` + countSheep(num - 1);
 }
 
-countSheep(3);
+// countSheep(3);
 
 
 //Array double
-
 
 function arrayDouble(arr) {
   //base case
@@ -24,4 +23,23 @@ function arrayDouble(arr) {
   let first = arr[0];
   return [first * 2, ...arrayDouble(arr.slice(1))]
 }
-console.log(arrayDouble([1,2,3]));
+// console.log(arrayDouble([1,2,3]));
+
+//Reverse String
+
+//Input - 'hello'
+//Output - 'olleh'
+
+const revStr = function(str) {
+  //Base case
+    if(str === '') {
+      return '';
+    }
+
+  //Recursive case
+    const lastChar = str.slice(-1); //--> 'o'
+
+    return lastChar + revStr(str.slice(0, str.length -1))
+}
+
+console.log(revStr("hello"));
